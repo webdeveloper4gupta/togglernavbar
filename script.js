cfcf6527c32004d131b5a670f93ceae7c64d55b5
Sukritan Gupta <20051871@kiit.ac.in>
2022-03-18 20:30:30 +0530

    completion

---
+++ script.js
@@ -0,0 +1,12 @@
+$("#amu2").click(()=>{
+    $("#sukritan1").toggle();
+});
+$("#sukritan2").click(()=>{
+    $("#mahajan1").show();
+    
+    
+})
+$("#maha1").click(()=>{
+    $("#mahajan1").hide();
+});
+
